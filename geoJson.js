3d360f2a459374df511c6b76d034289b882e4d48
Supashape <pascal.suhm@gmail.com>
2016-11-15 15:33:39 +0100

    XML added
    XML added

--- geoJson.js
+++ geoJson.js
@@ -1,7 +1,7 @@
 //save features of layer in Json format
 function inJson(){
 	var geoJSON = new OpenLayers.Format.GeoJSON();
-	var json = geoJSON.write( editingLayer.features );
-	console.log('json: ' +json);
+	json_var = geoJSON.write( editingLayer.features );
+	console.log('json: ' +json_var);
 }
 
