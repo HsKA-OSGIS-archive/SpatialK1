dff2c4461106e7f007ac181726548b75b9a1814f
Lilija Friesen <lili.friesen@gmail.com>
2016-12-06 10:44:35 +0100

    Delete Files

--- merge/merge/Scripts_own/geoJson.js
+++
@@ -1,7 +0,0 @@
-//save features of layer in Json format
-function inJson(){
-	var geoJSON = new OpenLayers.Format.GeoJSON();
-	json_var = geoJSON.write( editingLayer.features );
-	console.log('json: ' +json_var);
-}
-
