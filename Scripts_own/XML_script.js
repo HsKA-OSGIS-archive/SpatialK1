7694fd7e92156d0bda5b6ee35506f7585f2a668a
jeja1015 <jan.jedersberger@web.de>
2017-01-10 11:27:56 +0100

    Updated "Berechnete Strahlung"
    Updated "Berechnete Strahlung"

--- Scripts_own/XML_script.js
+++ Scripts_own/XML_script.js
@@ -1,12 +1,13 @@
 
 
-var v;
+
 var rbm;
 var foetus;
 var thyroid;
 var dose;
 var vx;
 
+// variable containing the response from the Server
 
 var response = {
 "red_bone_marrow": 0.021267209339325046,
@@ -15,22 +16,33 @@ var response = {
 "eff_dose": 0.024116610070867805
 };
 
-
+// reads the JSON from the layer in the map
 function inJson(){
 	var geoJSON = new OpenLayers.Format.GeoJSON();
 	json_var = geoJSON.write( editingLayer.features );
 	console.log('json: ' +json_var);
 }
 
+
+
+//converts the response JSON in values for the GUI
+//converts the unit in mSv 
+
 function readResponse(){
 	console.log("readresponse");
-	rbm = response.red_bone_marrow.toFixed(10);
-	foetus = response.foetus_1.toFixed(10);
-	thyroid = response.thyroid.toFixed(10);
-	dose = response.eff_dose.toFixed(10);
+	rbm = ((response.red_bone_marrow)*1000).toFixed(2);
+	rbm = rbm + " mSv";
+	foetus = ((response.foetus_1)*1000).toFixed(4);
+	foetus = foetus +  " mSv";
+	thyroid = ((response.thyroid)*1000).toFixed(2);
+	thyroid = thyroid + " mSv";
+	dose = ((response.eff_dose)*1000).toFixed(2);
+	dose = dose +" mSv";
 	
 }
 
+
+// creates the xml as String
 function createXML(){  
   
   
@@ -42,20 +54,20 @@ function createXML(){
 		'<wps:Input>'+
 			'<ows:Identifier>route</ows:identifier>'+
 			'<wps:Data>'+
-				'<wps:ComplexData mimeType="application/json"><![CDATA['+ json_var +']]></wps:ComplexData>'+
+				'<wps:ComplexData mimeType="application/json"><![CDATA['+ json_var +']]></wps:ComplexData>'+ //the JSON is included in the XML
 			'</wps:Data>'+
 		'</wps:Input>'+
 		'<wps:Input>'+
 			'<ows:Identifier>ageGroup</ows:identifier>'+
 			'<wps:Data>'+
-				'<wps:LiteralData>'+ ageGroup +
+				'<wps:LiteralData>'+ ageGroup + //the age variable is included in the XML
 				'</wps:LiteralData>'+
 			'</wps:Data>'+
 		'</wps:Input>'+
 		'<wps:Input>'+
 			'<ows:Identifier>stadiumFoetus</ows:identifier>'+
 			'<wps:Data>'+
-				'<wps:LiteralData>'+ stadiumFoetus +
+				'<wps:LiteralData>'+ stadiumFoetus + //the pregnancy variable is included in the XML
 				'</wps:LiteralData>'+
 			'</wps:Data>'+
 		'</wps:Input>'+
@@ -65,89 +77,15 @@ function createXML(){
   
   
   
- // vx= '<?xml version="1.0" encoding="UTF-8" standalone="true" ?><ows:identifier>gs:DoseReconstruction</ows:identifier><wps:DataInputs><wps:Input>'
+
   console.log("XML as String " +vx);
   
+  
+  // creates a XML-Object from the String
   parser = new DOMParser();
 	xml_var = parser.parseFromString(vx, "text/xml");
 	
-	console.log("XML as object " + xml_var);
+	//console.log("XML as object " + xml_var);
   
-  
-  //Old XML creation with XML Writer library
-  
-/*
-  
-  
-   v = new XMLWriter();
-   v.writeStartDocument(true);  
-   v.writeStartElement("Execute", "wps")
-		v.writeAttributeString('version','1.0.0');
-		v.writeAttributeString('service','WPS');
-		v.writeAttributeString('xmlns:xsi','http://www.w3.org/2001/XMLSchema-instance');
-		v.writeAttributeString('xmlns','http://www.opengis.net/wps/1.0.0');
-		v.writeAttributeString('xmlns:wfs','http://www.opengis.net/wfs');
-		v.writeAttributeString('xmlns:wps','http://www.opengis.net/wps/1.0.0');
-		v.writeAttributeString('xmlns:ows','http://www.opengis.net/ows/1.1');
-		v.writeAttributeString('xmlns:gml','http://www.opengis.net/gml');
-		v.writeAttributeString('xmlns:ogc','http://www.opengis.net/ogc');
-		v.writeAttributeString('xmlns:wcs','http://www.opengis.net/wcs/1.1.1');
-		v.writeAttributeString('xmlns:xlink','http://www.w3.org/1999/xlink');
-		v.writeAttributeString('xsi:schemaLocation','http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd');		
-   v.writeStartElement("identifier", "ows"); 
-   v.writeString("gs:DoseReconstruction");
-   v.writeEndElement();
-   v.writeStartElement("DataInputs", "wps");
-  
-   //JSON
-
-	   v.writeStartElement("Input", "wps"); 
-		   v.writeStartElement("identifier", "ows"); 
-		   v.writeString("route");
-		   v.writeEndElement();
-		   v.writeStartElement("Data", "wps");   
-				v.writeStartElement("ComplexData", "wps");
-				v.writeAttributeString('mimeType','application/json');				
-				v.writeCDATA(json_var);
-		   v.writeEndElement();
-		   v.writeEndElement();
-	   v.writeEndElement();
-
-
-  
-   
-   //AgeGroup
-
-	   v.writeStartElement("Input", "wps"); 
-		   v.writeStartElement("identifier", "ows"); 
-		   v.writeString("ageGroup");
-		   v.writeEndElement();
-		   v.writeStartElement("Data", "wps");   
-				v.writeStartElement("LiteralData", "wps"); 
-				v.writeString(ageGroup);
-		   v.writeEndElement();
-		   v.writeEndElement();
-	   v.writeEndElement();
- 
-   
-   
-   //Pregnant
-
-	   v.writeStartElement("Input", "wps"); 
-		   v.writeStartElement("identifier", "ows"); 
-		   v.writeString("stadiumFoetus");
-		   v.writeEndElement();
-		   v.writeStartElement("Data", "wps");   
-				v.writeStartElement("LiteralData", "wps"); 
-				v.writeString(stadiumFoetus);
-		   v.writeEndElement();
-		   v.writeEndElement();
-	   v.writeEndElement();
-	v.writeEndElement();
-   v.writeEndDocument();
-   v.writeEndDocument();
-   console.log('' + v);
-   console.log( v.flush() );
-*/
 }
 
